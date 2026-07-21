9ad8db2f54b8b427953fa8812b99abf977f759f4
dalmatinec <temirbekov1602@gmail.com>
2026-07-21 20:48:00 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,16 +5,16 @@ def main_menu():
     """Главное меню - инлайн кнопки"""
     buttons = [
         [
-            InlineKeyboardButton(text="🌺ᴄʜᴀᴛ", callback_data="link_chat"),
-            InlineKeyboardButton(text="🌺ɴᴇᴡs", callback_data="link_news")
+            InlineKeyboardButton(text="⚜𝐂𝐇𝐀𝐓", callback_data="link_chat"),
+            InlineKeyboardButton(text="⚜𝐍𝐄𝐖𝐒", callback_data="link_news")
         ],
         [
-            InlineKeyboardButton(text="🌺ʀᴇsᴇʀᴠ", callback_data="link_reserve"),
-            InlineKeyboardButton(text="🌺ʙᴏᴛ", url=get_link("bot"))
+            InlineKeyboardButton(text="⚜𝘽𝙊𝙏", url=get_link("bot")),
+            InlineKeyboardButton(text="⚜𝙒𝙚𝙗𝙨𝙖𝙮𝙩", url=get_link("website"))
         ],
         [
-            InlineKeyboardButton(text="🌺ᴍᴏʀᴇɴᴀ", url=get_link("ceo")),
-            InlineKeyboardButton(text="🌺ᴏᴘᴇʀᴀᴛᴏʀ", url=get_link("operator"))
+            InlineKeyboardButton(text="⚜𝙈𝙊𝙍𝙀𝙉𝘼", url=get_link("ceo")),
+            InlineKeyboardButton(text="⚜𝙊𝙋𝙀𝙍𝘼𝙏𝙊𝙍", url=get_link("operator"))
         ],
         [
             InlineKeyboardButton(text="✉️ Связаться с оператором", callback_data="support")
